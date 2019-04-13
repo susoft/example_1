a735023043d64a737aa38d6b95fd7785418c4c5b
woo <woo@woo-PC>
2019-04-13 10:11:11 +0900

    test

--- templateWebService/src/main/java/com/soo/temp/SecurityConfig.java
+++ templateWebService/src/main/java/com/soo/temp/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.soo.temp;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -9,11 +10,37 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    /*@Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+          .inMemoryAuthentication()
+          .withUser("admin").password("password").roles("ADMIN");
+    }*/
+    
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
             .anyRequest()
             .permitAll()
-            .and().csrf().disable();
+            .and().csrf().disable()
+        	/*.headers()
+            .addHeaderWriter(new StaticHeadersWriter("X-Content-Security-Policy","script-src 'self'"))
+            .frameOptions().disable()*/
+            ;
     }
+    
+
+	@Override
+	public void configure(WebSecurity web) throws Exception {
+		web.ignoring().antMatchers("/h2-console/**");
+	}
+    
+    /*@Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+          .httpBasic().and()
+          .authorizeRequests()
+          .antMatchers("/").hasRole("ADMIN")
+          .anyRequest().authenticated();
+    }*/
 }
